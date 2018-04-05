63cfb1d6736d22af68534dbddc3d870a63717d2d
clund12 <c.lund@protonmail.com>
2018-04-04 20:38:58 -0400

    New files

---
+++ Stepper-Motor-Micro.py
@@ -0,0 +1,55 @@
+from time import sleep
+import RPi.GPIO as GPIO
+import pigpio
+
+## START THE PIGPIO DEAMON! - sudo pigpiod
+# Default sampling rate (defines allowable frequencies) is 5. This can be
+# changed using the -s tag when calling pigpiod
+
+DIR = 20     # Direction GPIO Pin
+STEP = 21    # Step GPIO Pin
+SWITCH = 16  # GPIO Pin of Switch
+
+# Connect to pigpio daemon
+pi = pigpio.pi()
+
+# Set up pins as an output
+pi.set_mode(DIR, pigpio.OUTPUT)
+pi.set_mode(STEP, pigpio.OUTPUT)
+
+# Set up input switch
+pi.set_mode(SWITCH, pigpio.INPUT)
+pi.set_pull_up_down(SWITCH, pigpio.PUD_UP)
+
+# Note: current is 71% of maximum in full step mode, but this is not the case
+# for microstepping! If the current has been increased past the maximum when
+# we're working with full step, then we need to decrease it.
+MODE = (14,15,18) # Microstep Resolution GPIO pins
+#GPIO.setup(MODE, GPIO.OUT)
+
+RESOLUTION = {'Full': (0,0,0),
+        'Half': (1,0,0),
+        '1/4': (0,1,0),
+        '1/8': (1,1,0),
+        '1/16': (0,0,1),
+        '1/32': (1,0,1)}
+for i in range(3):
+    pi.wire(MODE[i],RESOLUTION['Full'][i])
+
+# Set duty cycle and frequency
+pi.set_PWM_dutycycle(STEP,128) # PWM 1/2 On 1/2 Off - 50% duty cycle
+pi.set_PWM_frequency(STEP,500) # 500 pulses per second - 500 Hz
+
+# For specific frequencies not in table PiStepperFreq.png, use pin 18 and:
+# pi.hardware_PWM(18,frequency,dutycycle)
+
+try:
+    while True:
+        pi.write(DIR,pi.read(SWITCH)) # Set direction
+        sleep(.1)
+
+except KeyboardInterrupt:
+    print("Stopping PIGPIO and exiting...")
+finally:
+    pi.set_PWM_dutycycle(STEP,0) # PWM Off
+    pi.stop()
---
+++ Stepper-Motor-Velocities.py
@@ -0,0 +1,37 @@
+import numpy as np
+from time import sleep
+import RPi.GPIO as GPIO # Can ONLY be run on a RPi!
+
+DIR = 20     # Direction GPIO Pin
+STEP = 21    # Step GPIO Pin
+CW = 1       # Clockwise Rotation
+CCW = 0      # Counterclockwise Rotation
+SPR = 200     # Steps per Revolution (Checked according to NEMA 17 spec sheet)
+
+GPIO.setmode(GPIO.BCM)      # Broadcom memory
+GPIO.setup(DIR, GPIO.OUT)   # Sets direction pin to an output
+GPIO.setup(STEP, GPIO.OUT)  # Sets step pin to an output
+GPIO.output(DIR, CW)        # Sets initial direction to clockwise
+
+step_count = SPR    # Set initial test to a single rotation
+delay = 0.005      # (1s / 200) - rotation will take 1 second if no delay
+
+#### Call this file via the command line, passing the desired csv file as an argument: >> python TracetoVelocities.py sometrace.csv
+
+# Read in the file -> gives a list of values like ['x1,y1\n','x2,y2\n',etc.]
+data = open(sys.argv[1],'r')
+data = data.readlines()
+
+# Create new list of (x1,y1),(x2,y2), etc. tuples
+xy = []
+for line in data:
+    line = line.strip('\n')
+    thisxy = line.split(',')
+    xy.append(thisxy)
+
+# Separate list of tuples into lists of just x, y
+x, y = zip(*xy)
+
+# Convert x, y into lists of numbers
+x = list(map(float,x))
+velocities = list(map(float,y))
