f2f77a9e3cbe352df749d322b40ef5f889d60d06
clund12 <c.lund@protonmail.com>
2018-04-26 17:38:37 -0400

    New trace.csv

--- TraceGenerator.py
+++ TraceGenerator.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 import csv
 
 # Define a sine wave
-x = np.linspace(0, 2*sc.pi, 100)
-xy = list(map(lambda i: [i, 4*np.sin(i)], x))
+x = np.linspace(0, 12*sc.pi, 100)
+xy = list(map(lambda i: [10*i/(2*sc.pi), 4*np.sin(i)], x))
 
 ##### Place any other function you want to test here:
 
 
 # Plot whatever trace you want to verify it's of the right form, is sufficiently smooth, etc.
 
-plt.plot(*zip(*xy))
-plt.show()
+#plt.plot(*zip(*xy))
+#plt.show()
 
 # If it's good, comment out the plot and uncomment the lines below to write it to a csv file
 
-#with open('trace.csv','w') as output:
-#    writer = csv.writer(output)
-#    writer.writerows(xy)
+with open('trace.csv','w') as output:
+    writer = csv.writer(output)
+    writer.writerows(xy)
