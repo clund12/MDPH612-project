23fd53f692f4591bce47462d644b939bc89180cb
clund12 <c.lund@protonmail.com>
2018-04-26 18:30:10 -0400

    new square trace

--- SquareTrace.py
+++ SquareTrace.py
@@ -3,7 +3,7 @@ import csv
 from scipy import signal
 
 x = np.linspace(0,10,100)
-xy = list(map(lambda i: [i, signal.square(i)], x))
+xy = list(map(lambda i: [i, signal.square(2*np.pi*5*i)], x))
 
 with open('squaretrace.csv','w') as output:
     writer = csv.writer(output)
